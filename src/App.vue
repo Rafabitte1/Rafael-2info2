52eae91081e40ad3f5355a35f0484838a3981d4b
Rafabitte1 <rafabitte1@gmail.com>
2023-04-28 16:13:45 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,119 @@
-
-
-<head>
-    <title>Formulário de Edição de Perfil</title>
-</head>
-<body>
-    <h1>Formulário de Edição de Perfil</h1>
-    <form action="processa_formulario.php" method="post">
-        <label for="nome">Nome:</label>
-        <input type="text" id="nome" name="nome" required><br><br>
-
-        <label for="email">E-mail:</label>
-        <input type="email" id="email" name="email" required><br><br>
-
-        <label for="senha">Senha:</label>
-        <input type="password" id="senha" name="senha" required><br><br>
-
-        <label for="confirmacao_senha">Confirmação de Senha:</label>
-        <input type="password" id="confirmacao_senha" name="confirmacao_senha" required><br><br>
-
-        <label for="data_nascimento">Data de Nascimento:</label>
-        <input type="date" id="data_nascimento" name="data_nascimento" required><br><br>
-
-        <label for="endereco">Endereço:</label>
-        <input type="text" id="endereco" name="endereco" required><br><br>
-
-        <label for="cidade">Cidade:</label>
-        <input type="text" id="cidade" name="cidade" required><br><br>
-
-        <label for="estado">Estado:</label>
-        <select id="estado" name="estado" required>
-            <option value="AC">Acre</option>
-            <option value="AL">Alagoas</option>
-            <option value="AP">Amapá</option>
-            <option value="AM">Amazonas</option>
-            <option value="BA">Bahia</option>
-            <option value="CE">Ceará</option>
-            <option value="DF">Distrito Federal</option>
-            <option value="ES">Espírito Santo</option>
-            <option value="GO">Goiás</option>
-            <option value="MA">Maranhão</option>
-            <option value="MT">Mato Grosso</option>
-            <option value="MS">Mato Grosso do Sul</option>
-            <option value="MG">Minas Gerais</option>
-            <option value="PA">Pará</option>
-            <option value="PB">Paraíba</option>
-            <option value="PR">Paraná</option>
-            <option value="PE">Pernambuco</option>
-            <option value="PI">Piauí</option>
-            <option value="RJ">Rio de Janeiro</option>
-            <option value="RN">Rio Grande do Norte</option>
-            <option value="RS">Rio Grande do Sul</option>
-            <option value="RO">Rondônia</option>
-            <option value="RR">Roraima</option>
-            <option value="SC">Santa Catarina</option>
-            <option value="SP">São Paulo</option>
-            <option value="SE">Sergipe</option>
-            <option value="TO">Tocantins</option>
-        </select><br><br>
-
-        <label for="hobbies">Hobbies:</label>
-        <input type="text" id="hobbies" name="hobbies"><br><br>
-
-        <label for="linguagens
-
-
-// Seleciona o formulário
-const form = document.querySelector('form');
-
-// Seleciona os campos de senha e confirmação de senha
-const senha = form.querySelector('#senha');
-const confirmaSenha = form.querySelector('#confirma-senha');
-
-// Adiciona um evento de envio de formulário
-form.addEventListener('submit', (event) => {
-  // Verifica se a senha e a confirmação de senha são iguais
-  if (senha.value !== confirmaSenha.value) {
-    // Se forem diferentes, impede o envio do formulário
-    event.preventDefault();
-    alert('As senhas não coincidem. Por favor, verifique e tente novamente.');
+<style>
+form {
+    max-width: 505px;
+    margin: 0 auto;
   }
-});
+  
+  label {
+    display: block;
+    margin-bottom: 10px;
+    font-weight: bold;
+  }
+  
+  
+  input[type="text"],
+  input[type="email"],
+  input[type="password"],
+  select,
+  textarea {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #d4caca;
+    border-radius: 4px;
+  }
+  
+  select {
+    height: 40px;
+  }
+  
+  textarea {
+    height: 100px;
+  }
+  
+  
+  input[type="submit"] {
+    display: block;
+    width: 100%;
+    padding: 10px;
+    background-color: #245d99;
+    color: #beb4b4;
+    text-align: center;
+    cursor: pointer;
+    border: none;
+    border-radius: 4px;
+    font-size: 16px;
+  }
+  
+  
+  br {
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+  </style>
+ 
+    <script src="main.js"></script>
+ 
+    
+        <title>Formulário de Edição de Perfil</title>
+    <head></head>
+<template>
+        <h1>Formulário de Edição de Perfil</h1>
+        
+        <form action="processa_formulario.php" method="post">
+  <label for="nome">Nome:</label>
+  <input type="text" id="nome" name="nome" required><br><br>
+
+  <label for="email">E-mail:</label>
+  <input type="email" id="email" name="email" required><br><br>
+
+  <label for="senha">Senha:</label>
+  <input type="password" id="senha" name="senha" required><br><br>
+
+  <label for="confirmacao_senha">Confirmação de Senha:</label>
+  <input type="password" id="confirmacao_senha" name="confirmacao_senha" required><br><br>
+
+  <label for="data_nascimento">Data de Nascimento:</label>
+  <input type="date" id="data_nascimento" name="data_nascimento" required><br><br>
+
+  <label for="endereco">Endereço:</label>
+  <input type="text" id="endereco" name="endereco" required><br><br>
+
+  <label for="cidade">Cidade:</label>
+  <input type="text" id="cidade" name="cidade" required><br><br>
+
+  <label for="estado">Estado:</label>
+  <select id="estado" name="estado" required>
+    <option value="AC">Acre</option>
+    <option value="AL">Alagoas</option>
+    <option value="AP">Amapá</option>
+    <option value="AM">Amazonas</option>
+    <option value="BA">Bahia</option>
+    <option value="CE">Ceará</option>
+    <option value="DF">Distrito Federal</option>
+    <option value="ES">Espírito Santo</option>
+    <option value="GO">Goiás</option>
+    <option value="MA">Maranhão</option>
+    <option value="MT">Mato Grosso</option>
+    <option value="MS">Mato Grosso do Sul</option>
+    <option value="MG">Minas Gerais</option>
+    <option value="PA">Pará</option>
+    <option value="PB">Paraíba</option>
+    <option value="PR">Paraná</option>
+    <option value="PE">Pernambuco</option>
+    <option value="PI">Piauí</option>
+    <option value="RJ">Rio de Janeiro</option>
+    <option value="RN">Rio Grande do Norte</option>
+    <option value="RS">Rio Grande do Sul</option>
+    <option value="RO">Rondônia</option>
+    <option value="RR">Roraima</option>
+    <option value="SC">Santa Catarina</option>
+    <option value="SP">São Paulo</option>
+    <option value="SE">Sergipe</option>
+    <option value="TO">Tocantins</option>
+  </select><br><br>
+
+  <label for="hobbies">Hobbies:</label>
+  <input type="text" id="hobbies" name="hobbies"><br><br>
+
+ 
--- src/main.js
+++ src/main.js
@@ -4,3 +4,81 @@ import App from './App.vue'
 import './assets/main.css'
 
 createApp(App).mount('#app')
+createApp(App).mount('#app')
+function validateForm() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirm-password").value;
+    const birthdate = document.getElementById("birthdate").value;
+    const address = document.getElementById("address").value;
+    const city = document.getElementById("city").value;
+    const state = document.getElementById("state").value;
+    let errors = [];
+    if (!name.trim()) {
+      errors.push("Nome é obrigatório");
+    }
+    if (!email.trim()) {
+      errors.push("E-mail é obrigatório");
+    } else if (!isValidEmail(email)) {
+      errors.push("E-mail inválido");
+    }
+    if (!password.trim()) {
+      errors.push("Senha é obrigatória");
+    } else if (password.length < 8) {
+      errors.push("Senha deve ter no mínimo 8 caracteres");
+    }
+    if (password !== confirmPassword) {
+      errors.push("Senhas não conferem");
+    }
+    if (!birthdate.trim()) {
+      errors.push("Data de Nascimento é obrigatória");
+    } else if (!isValidDate(birthdate)) {
+      errors.push("Data de Nascimento inválida");
+    }
+    if (!address.trim()) {
+      errors.push("Endereço é obrigatório");
+    }
+    if (!city.trim()) {
+      errors.push("Cidade é obrigatória");
+    }
+    if (!state.trim()) {
+      errors.push("Estado é obrigatório");
+    }
+    if (errors.length > 0) {
+      const errorContainer = document.getElementById("error-container");
+      errorContainer.innerHTML = "";
+      for (let i = 0; i < errors.length; i++) {
+        const errorMessage = document.createElement("div");
+        errorMessage.className = "error";
+        errorMessage.innerHTML = errors[i];
+        errorContainer.appendChild(errorMessage);
+      }
+      return false;
+    }
+    return true;
+  }
+  function isValidEmail(email) {
+    const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    return emailRegex.test(email);
+  }
+  function isValidDate(birthdate) {
+    const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+    if (!dateRegex.test(birthdate)) {
+      return false;
+    }
+    const parts = birthdate.split("-");
+    const year = parseInt(parts[0], 10);
+    const month = parseInt(parts[1], 10);
+    const day = parseInt(parts[2], 10);
+    if (year < 1900 || year > new Date().getFullYear()) {
+      return false;
+    }
+    if (month < 1 || month > 12) {
+      return false;
+    }
+    if (day < 1 || day > 31) {
+      return false;
+    }
+    return true;
+  }
